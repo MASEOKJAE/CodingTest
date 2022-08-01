6c9b57c657cc5c8921c316d1a50b5b396e2985d6
ma_seokjae <79703790+MASEOKJAE@users.noreply.github.com>
2022-08-01 13:35:51 +0900

    Update and rename planting.cpp to headOfVillage.cpp

--- 4Week/headOfVillage.cpp
+++ 4Week/headOfVillage.cpp
@@ -1,29 +1,12 @@
 #include <iostream>
 #include <string>
+#include <vector>
+#include <algorithm>
 
 using namespace std;
 
-// 내림차순으로 나무 자라는 날짜 sort
-void sorting(int loop, int tOrigin[], int tUpdate[]) {
-    int count = 0;
-    while(true) {
-        int max = 0, m_index = 0;
-        for(int i=0; i<loop; i++) {
-            if(max < tOrigin[i]) {
-                max = tOrigin[i];
-                m_index = i;
-            }
-        }
-        tUpdate[count] = max;
-        tOrigin[m_index] = 0;
-        count++;
-        if(count == loop)
-            break;
-    }
-}
-
 // 나무가 다 자라는 날짜
-void inviteDay(int loop, int tUpdate[], int &day) {
+void inviteDay(int loop, vector<int> tUpdate, int &day) {
     for(int i=0; i<loop; i++) {
         if(i == 0) // 자라는 날짜값 초기화
             day = tUpdate[i] + (i+1); // i+1을 해주는 이유는 loop가 0부터 시작하기 떄문에
@@ -38,17 +21,22 @@ void inviteDay(int loop, int tUpdate[], int &day) {
 int main() {
     int tNum = 0, dDay = 0;
     cin >> tNum;
-    int tType[tNum], tSort[tNum];
+    vector<int> tType;
 
-    for(int i=0; i<tNum; i++)
-        cin >> tType[i];
+    for(int i=0; i<tNum; i++) {
+        int temp = 0;
+        cin >> temp;
+        tType.push_back(temp);
+    }
 
-    sorting(tNum, tType, tSort);
-    inviteDay(tNum, tSort, dDay);
+    // 내림차순으로 나무 자라는 날짜 sort
+    sort(tType.rbegin(), tType.rend());
+
+    inviteDay(tNum, tType, dDay);
 
     // 이장님 초대는 나무가 다 자란 다음날
     dDay++;
-
+    
     // 결과 출력
     cout << dDay << endl;
 }
